aa3f0b795394420fbab6725f3e7eb52729f29dbb
mateusz <mateusz@desktop-990kho2.home>
2024-10-10 21:06:45 +0200

    easy

---
+++ 1.two-sum.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i],i);
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            int possible = target - nums[i];
+            if(map.containsKey(possible) && map.get(possible) != i){
+                result = new int[]{i,map.get(target-nums[i])};
+            }
+        }
+
+        return result;
+    }
+}
+// @lc code=end
+
